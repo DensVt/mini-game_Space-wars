f995146cac62c218550ea8a6db4049f2eb81f876
Denis Mtv <Freeflop@outlook.com>
2023-02-21 14:56:16 +0300

    bullet rendering added

--- controls.py
+++ controls.py
@@ -1,6 +1,7 @@
 import pygame, sys
+from bullet import Bullet
 
-def events(gun):
+def events(screen, gun, bullets):
     """ Обработка событий """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,6 +13,9 @@ def events(gun):
                 gun.mright = True
             elif event.key == pygame.K_a:
                 gun.mleft = True
+            elif event.key == pygame.K_SPACE:
+                new_bullet = Bullet(screen, gun)
+                bullets.add(new_bullet)
 
         elif event.type == pygame.KEYUP:
             # Движение влево
@@ -19,3 +23,24 @@ def events(gun):
                 gun.mright = False
             elif event.key == pygame.K_a:
                 gun.mleft = False
+
+def update(bg_color, screen, gun, bullets):
+    """ Обновление экрана """
+    screen.fill(bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    gun.output()
+    pygame.display.flip()
+
+def update_bullets(bullets):
+    """ Обновление позиции пулек"""
+    bullets.update()
+
+    # удаление пулек за пределами экрана
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    
+    # вывод длины контеннера с пульками и их мгновенное удаление
+    # print(len(bullets))
+
--- gun.py
+++ gun.py
@@ -10,6 +10,10 @@ class Gun():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
+
+        # преобразование, т.к rect раюотает с целыми числами.
+        # сделано для более плавного движения обьекта
+        self.center = float(self.rect.centerx)
         self.rect.bottom = self.screen_rect.bottom
         self.mright = False
         self.mleft = False
@@ -21,6 +25,8 @@ class Gun():
     def update_gun(self):
         """ Обновление позиции пушки """
         if self.mright and self.rect.right < self.screen_rect.right: # проверка выхода за пределы поля
-            self.rect.centerx += 1
+            self.center += 1.5
         if self.mleft and self.rect.left > 0: # проверка выхода за пределы поля
-            self.rect.centerx -= 1
+            self.center -= 1.5
+
+        self.rect.centerx = self.center
