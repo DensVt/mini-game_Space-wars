c74b9524b9fb78f71ae8e7e9bdf16fc54640d7f1
Denis Mtv <Freeflop@outlook.com>
2023-02-21 14:18:34 +0300

    Object position update added

--- gun.py
+++ gun.py
@@ -11,7 +11,16 @@ class Gun():
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.mright = False
+        self.mleft = False
 
     def output(self):
         """ Прорисовка пушки """
         self.screen.blit(self.image, self.rect)
+
+    def update_gun(self):
+        """ Обновление позиции пушки """
+        if self.mright and self.rect.right < self.screen_rect.right: # проверка выхода за пределы поля
+            self.rect.centerx += 1
+        if self.mleft and self.rect.left > 0: # проверка выхода за пределы поля
+            self.rect.centerx -= 1
--- space_game.py
+++ space_game.py
@@ -1,19 +1,16 @@
-import pygame
-import sys
+import pygame, controls
 from gun import Gun
 
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    screen = pygame.display.set_mode((700, 600))
     pygame.display.set_caption("Space wars")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
+        controls.events(gun)
+        gun.update_gun()
         screen.fill(bg_color)
         gun.output()
         gun.output()
